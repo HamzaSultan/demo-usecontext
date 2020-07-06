e16b3c1cdac2b29514fc68fedc01706f238df6ad
Ameer Hamza Sultan <56472325+HamzaSultan@users.noreply.github.com>
2020-07-06 19:22:54 +0500

    Update numberReducer.js

--- src/numberReducer.js
+++ src/numberReducer.js
@@ -1,9 +1,11 @@
-const numberReducer = (state, action) => {
+const numberReducer =
+      (state, action) => {
+    
     switch (action.type) {
       case 'INCREMENT':
         return state + action.val
       case 'DECREMENT':
         return state - action.val
-    }
-  }
+    };
+  };
   export default numberReducer;
